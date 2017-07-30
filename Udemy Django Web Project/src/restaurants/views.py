0161658efb974d03a558d4942a5091e1c45731f4
Ozaren <krishna.sd.2012@gmail.com>
2017-07-30 17:24:14 -0400

    Using Templates Inheritance

--- Udemy Django Web Project/src/restaurants/views.py
+++ Udemy Django Web Project/src/restaurants/views.py
@@ -7,4 +7,15 @@ import random as rand
 
 def home(request):
     num = rand.randint(0, 1000000)
-    return render(request, "base.html" , {"bool": False , "num": num})
+    context = {"bool": False , "num": num}
+    return render(request, "home1.html" , context)
+
+def home2(request):
+    num = rand.randint(0, 1000000)
+    context = {}
+    return render(request, "home2.html" , context)
+
+def home3(request):
+    num = rand.randint(0, 1000000)
+    context = {}
+    return render(request, "home3.html" , context)
